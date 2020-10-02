109d77c5a5a43ed0aa1641195a759e444f3d0fcc
SunFlow <sunflow.riven@gmai.com>
2020-10-03 00:19:18 +0200

    improved overlay video
    + the overlay video now snaps to the border of the window
    + the overlay video now always has the aspect ratio 16:9
    + the overlay video now becomes interactable by double clicking on it

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Created so you can watch a Stream with another Stream on top without ugly border
 
 [Ls WorldsCoStream](https://www.multistream.ml/ls-co-stream?main=riotgames&overlay=imls)
 
-[Ls WorldsCoStream NEW](https://www.multistream.ml/ls-co-stream/new?main=riotgames&overlay=imls&maintype=twitch&overlaytype=twitch)
+[Ls WorldsCoStream NEW](https://www.multistream.ml/ls-co-stream/new?main=riotgames&overlay=imls&maintype=twitch&overlaytype=twitch) (you drag, resize the overlayed stream, to interact just double click onto it)
--- ls-co-stream/new/sketch.js
+++ ls-co-stream/new/sketch.js
@@ -1,56 +1,80 @@
+let interactOverlay = false;
+let dblce = false;
+
 function setup() {
-  $(".draggable").draggable();
-  $(".resizable").resizable();
+  let h = window.overlay.getBoundingClientRect().height;
+  window.overlay.style.height = h + "px";
+  window.overlay.style.width= (h * 1.77777777778) + "px";
+
+  $("#border").draggable();
+  $("#overlay").draggable({
+    snap : true,
+    snapMode: "inner",
+    snapTolerance: 5
+ });
+  $("#overlay").resizable({
+    aspectRatio: true
+  });
 
   let parent = "www.multistream.ml";
-  
+  // parent = "127.0.0.1";
+
   let mainType = getUrlParam('maintype', 'twitch');
   let overlayType = getUrlParam('overlaytype', 'twitch');
-  
+
   let mainChannel = getUrlParam('main', 'riotgames');
   let overlayChannel = getUrlParam('overlay', 'imls');
-					 
+
   let mainFirst = getTY(mainType, "https://player.twitch.tv/?channel=", "https://www.youtube.com/embed/");
   let overlayFirst = getTY(overlayType, "https://player.twitch.tv/?channel=", "https://www.youtube.com/embed/");
-  					 
+
   let mainLast = getTY(mainType, "&parent=" + parent, "");
   let overlayLast = getTY(overlayType, "&parent=" + parent, "");
 
+  window.mainVideo.src = mainFirst + mainChannel + mainLast;
+  window.overlayVideo.src = overlayFirst + overlayChannel + overlayLast;
+}
 
-  window.embed.src = mainFirst + mainChannel + mainLast;
-  window.overlay.src = overlayFirst + overlayChannel + overlayLast;
+function dblclickedEvent() {
+  interactOverlay = true;
+  dblce = true;
+  window.dragger.style.zIndex = 2;
+  window.overlayVideo.style.opacity = "100%";
 }
 
 function getTY(type, twitch, youtube) {
-  return type == "twitch" ? twitch : 
+  return type == "twitch" ? twitch :
 		 type == "youtube" ? youtube :
 		 "";
 }
 
-function onOver() {
-  if (!keyIsDown(CONTROL)) window.dragover.style.zIndex = 1;
+function overEvent() {
+  if (!interactOverlay){
+    window.dragger.style.zIndex = 4;
+    window.overlayVideo.style.opacity = "75%";
+  }
 }
 
-function onOut() {
-  window.dragover.style.zIndex = -1;
-}
-
-function keyPressed() {
-  window.dragover.style.zIndex = -1;
+function outEvent() {
+  if(!dblce) {
+    window.dragger.style.zIndex = 2;
+    window.overlayVideo.style.opacity = "100%";
+    interactOverlay = false;
+  } else dblce = false;
 }
 
 function getUrlParam(parameter, defaultvalue) {
-  var urlparameter = defaultvalue;
+  let urlparameter = defaultvalue;
   if (window.location.href.indexOf(parameter) > -1) {
-	urlparameter = getUrlVars()[parameter];
+	   urlparameter = getUrlVars()[parameter];
   }
   return urlparameter;
 }
 
 function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
-	vars[key] = value;
+  let vars = {};
+  let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+	   vars[key] = value;
   });
   return vars;
 }
